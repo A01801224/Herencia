a1705eeeb41d348442b58ef15b91c10c0b3bfc7e
A01801224 <149620528+A01801224@users.noreply.github.com>
2024-05-20 13:32:15 +0000

    ef

---
+++ main.cpp
@@ -0,0 +1,16 @@
+class Padre
+{
+    public : 
+    int x = 5;
+
+    void imprimir ()
+    {
+        cout << "La variable x:" << x << endl; 
+    }
+};
+
+int main ()
+{
+    Padre p;
+    return 0;
+}
